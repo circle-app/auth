b34d0a7f42cfc365d753805450530c297f6c71dd
pando85 <pando855@gmail.com>
2018-12-26 00:27:40 +0100

    Add scope field for user auth

--- test/test_handlers.py
+++ test/test_handlers.py
@@ -42,7 +42,7 @@ async def test_auth_incorret_password(cli):
 async def test_user_add(cli):
     token = await get_token(cli)
     auth_header = {'Authorization': f'Bearer {token}'}
-    user = {'username': 'test2', 'password': 'test1234'}
+    user = {'username': 'test2', 'password': 'test1234', 'scope': 'user'}
     resp = await cli.post(f'{BASE_URL}/user', json=user, headers=auth_header)
     assert resp.status == 201
     assert await resp.json() == user
